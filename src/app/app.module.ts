010c351f931f1f37975f17577541b21feccfac91
Baran Üçdağ <75309438+baranucdag@users.noreply.github.com>
2021-08-14 20:55:35 +0300

    14.08.2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColourComponent } from './components/colour/colour.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    BrandComponent,
+    ColourComponent,
+    CustomerComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/colour/colour.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colour',
+  templateUrl: './colour.component.html',
+  styleUrls: ['./colour.component.css']
+})
+export class ColourComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
