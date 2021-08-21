cd9949d30892c8f80e50b74506b523310f8d932f
Baran Üçdağ <75309438+baranucdag@users.noreply.github.com>
2021-08-21 13:24:32 +0300

    21.08.2021

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { RentalComponent } from './components/rental/rental.component';
+import { CarComponent } from './components/car/car.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",pathMatch:"full",component:CarComponent},
+  {path:"cars",component:CarComponent },
+  {path:"rentals",component:RentalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
