d89020a77ed477c74b8208134055f2a63b8cdf36
Baran Üçdağ <75309438+baranucdag@users.noreply.github.com>
2021-08-23 20:00:15 +0300

    brand list feature added.

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -1,6 +1,6 @@
+import { Brand } from './../../models/brand';
 import { BrandService } from './../../services/brand.service';
 import { Component, OnInit } from '@angular/core';
-import { Brand } from 'src/app/models/brand';
 
 @Component({
   selector: 'app-brand',
@@ -9,8 +9,9 @@ import { Brand } from 'src/app/models/brand';
 })
 export class BrandComponent implements OnInit {
 
-  constructor(private brandService:BrandService) { }
   brands:Brand[]=[];
+  currentBrand :Brand;
+  constructor(private brandService:BrandService) { }
 
   ngOnInit(): void {
     this.getBrands();
@@ -20,6 +21,18 @@ export class BrandComponent implements OnInit {
     this.brandService.getBrands().subscribe(response=>{
       this.brands=response.data;
     })
+  }
+
+  setCurrentBrand(brand:Brand){
+    this.currentBrand=brand;
+  }
+
+  getCurrentBrandClass(brand:Brand){
+    if(brand==this.currentBrand){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
   }
 
 }
